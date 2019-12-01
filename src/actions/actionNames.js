303fa80d235e003d4461fac05f0463e964d64452
Caroline Desprat <git@desprat.fr>
2019-12-01 23:10:19 +0100

    actions

---
+++ src/actions/actionNames.js
@@ -0,0 +1,10 @@
+/**
+ * Actions names
+ * 
+ * This file contains all the action names that will be used 
+ * in the app. They are exported as variable to facilitate reuse without typos
+ */
+
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const CHANGE_VISIBILITY_FILTER = "CHANGE_VISIBILITY_FILTER";
