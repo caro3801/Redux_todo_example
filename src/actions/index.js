c6c58f754fbf08c79f12e31f0647e235cddb5f12
Caroline Desprat <git@desprat.fr>
2019-12-01 23:10:19 +0100

    actions

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+/**
+ * Actions 
+ * 
+ * Each function returns a pure object called an action in Redux
+ */
+
+import { ADD_TODO, TOGGLE_TODO, CHANGE_VISIBILITY_FILTER } from "../actions/actionNames";
+
+// this is a helper to Id the created todos
+let nextTodoId = 0;
+
+export const addTodo = text => ({
+  type: ADD_TODO,
+  id: nextTodoId++,
+  text
+});
+
+export const toggleTodo = id => ({
+  type: TOGGLE_TODO,
+  id
+});
+
+export const changeFilter = filter => ({
+  type: CHANGE_VISIBILITY_FILTER,
+  filter
+});
